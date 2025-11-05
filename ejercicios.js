e0cf3ec359ac1affca268e73810d443c58518013
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2025-11-05 11:40:26 -0600

    Ejercicio 7: Resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -110,7 +110,13 @@ function evaluarNota(nota) {
  */
 function mayorDeTres(a, b, c) {
   // TODO: Encuentra y retorna el mayor de los tres nÃºmeros
-  return 0;
+  if (a>=b && a>=c){
+    return a
+  } else if (b>=a && b>=c){
+  return b
+  } else {
+    return c
+  }
 }
 
 /**
