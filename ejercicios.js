1f006ff1cce303c6e832839ddb60766a8836d26b
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2025-11-07 21:57:35 -0600

    Ejercicio 22: Resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -364,13 +364,19 @@ function obtenerColumna(matriz, indiceColumna) {
  * La transpuesta intercambia filas por columnas
  * @param {Array<Array<number>>} matriz - Matriz original
  * @returns {Array<Array<number>>} - Matriz transpuesta
- * Ejemplo: transponer([[1, 2, 3], [4, 5, 6]]) debe retornar [[1, 4], [2, 5], [3, 6]]
  */
 function transponer(matriz) {
-  // TODO: Crea la matriz transpuesta
-  const transpuesta = [];
+  transpuesta = [];
+  for (let i = 0; i < matriz[0].length; i++) {
+    fila = [];
+    for (let j = 0; j < matriz.length; j++) {
+      fila.push(matriz[j][i]);
+    }
+    transpuesta.push(fila);
+  }
   return transpuesta;
 }
+
 
 // ============================================
 // NO MODIFIQUES ESTA LÃNEA - NECESARIA PARA LOS TESTS
