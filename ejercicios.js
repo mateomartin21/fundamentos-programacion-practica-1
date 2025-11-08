1b1e8fe8c41f68358bab0992b02f692e6b7f8fe4
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2025-11-07 21:43:27 -0600

    EJercicicio 15: Resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -244,14 +244,18 @@ function promedioArray(numeros) {
 }
 
 /**
- * Ejercicio 4.3: Encontrar el mÃ¡ximo (6 puntos)
- * @param {Array<number>} numeros - Array de nÃºmeros
- * @returns {number} - El nÃºmero mÃ¡s grande del array
+ * Ejercicio 4.3: Encontrar el máximo (6 puntos)
+ * @param {Array<number>} numeros - Array de números
+ * @returns {number} - El número más grande del array
  */
 function encontrarMaximo(numeros) {
-  // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
-  // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  return 0;
+  let maximo = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > maximo) {
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
 }
 
 /**
